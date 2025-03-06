7e3a53aed32da297aa33769f01fedbbed703b11c
Pranish Chaulagain <cpranishabc163@gmail.com>
2025-03-06 20:28:56 +0545

    Change default port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
 
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 
 // connect to mongodb
 connectDB();
